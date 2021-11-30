23d48c3d1a7acd332fc8fed6b6836b158d9f729c
Grayson Armour <50249088+garmour12@users.noreply.github.com>
2021-11-29 20:30:42 -0800

    Q-learning script & associated constants

---
+++ constant.py
@@ -0,0 +1,21 @@
+# CONSTANTS FOR QLEARN.PY
+
+# File paths to data
+SMALL_FP = "data/small.csv"
+MEDIUM_FP = "data/small.csv"
+LARGE_FP = "data/large.csv"
+
+# Discount factors
+SMALL_GAMMA = 0.95
+MEDIUM_GAMMA = 1
+LARGE_GAMMA = 0.95
+
+# Sizes of state spaces
+SMALL_N_STATES = 100
+MEDIUM_N_STATES = 50000
+LARGE_N_STATES = 312020
+
+# Sizes of action spaces
+SMALL_N_ACTIONS = 4
+MEDIUM_N_ACTIONS = 7
+LARGE_N_ACTIONS = 9
