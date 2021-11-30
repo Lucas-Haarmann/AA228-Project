611565e3d923ce01776f1ed15e7ad595c5b3f381
Devansh Sharma <devansh.sharma2807@gmail.com>
2021-11-29 21:23:11 -0800

    Devansh Q-learning

--- qlearn.py
+++ qlearn.py
@@ -1,105 +1,45 @@
-from pandas import read_csv
-from random import randint
-import constant as const
+import random
 import sys
-import timeit
+import math
 import numpy as np
+import pandas as pd
+from pandas.core.base import NoNewAttributesMixin
+import matplotlib.pyplot as plt
+import time
 
-def load_data():
-    """
-    LOAD DATA:
-    Uses command line argument to select and load the correct data file. Valid command line arguments are "large.csv", 
-    "medium.csv", and "small.csv". If an invalid argument is given, defaults to "small.csv". If no arguments are given,
-    raises system error.
-    Return: pandas dataframe with data loaded 
-    Return: gamma: (float) discount factor for loaded data
-    Return: n_states: (int) size of state space
-    Return: n_actions: (int) size of action space
-    """
-    try:
-        arg = sys.argv[1]
-    except IndexError:
-        raise SystemExit("Please enter a valid argument: small.csv, medium.csv, or large.csv")
+def Q_learning (data, Q, discount, lr, iterations):
+    for _ in range(iterations):
+        for index, row in data.iterrows():
+            s = row['s']
+            a = row['a']
+            s_prime = row['sp']
+            reward = row['r']
+            curr = Q[s,a]
+            Q[s,a] = curr + lr * (reward + discount * np.max(Q[s_prime,:]) - curr)
+    policy = np.argmax(Q, axis = 1)
+    return policy
 
-    if arg == "large.csv":
-        data_fp = const.LARGE_FP
-        gamma = const.LARGE_GAMMA
-        n_states = const.LARGE_N_STATES
-        n_actions = const.LARGE_N_ACTIONS
-    elif arg == "medium.csv":
-        data_fp = const.MEDIUM_FP
-        gamma = const.MEDIUM_GAMMA
-        n_states = const.MEDIUM_N_STATES
-        n_actions = const.MEDIUM_N_ACTIONS
+def compute(infile, outfile):
+    start = time.time()
+    D = pd.read_csv(infile)
+    S, A, discount, lr, iterations = 0, 2, 0.95, 1e-1, 2
+    if (infile == 'small.csv'):
+        S, iterations = 100, 2
+    elif (infile == 'medium.csv'):
+        S, iterations = 10000, 2
     else:
-        data_fp = const.SMALL_FP
-        gamma = const.SMALL_GAMMA
-        n_states = const.SMALL_N_STATES
-        n_actions = const.SMALL_N_ACTIONS
-    
-    return read_csv(data_fp), gamma, n_states, n_actions
-
-def update(Q, s, a, r, sp, alpha, gamma):
-    """
-    UPDATE:
-    Updates action value function Q given information about a state transition and its reward.
-    Param: Q: (numpy array of size (n_states, n_actions)) action value function
-    Param: s: (int) current state
-    Param: a: (int) action
-    Param: r: (float) state transition reward
-    Param: sp: (int) next state
-    Param: alpha: (float) learning rate
-    Param: gamma: (float) discount factor
-    Returns: implicit changes to Q
-    """
-    Q[s-1,a-1] += alpha * (r + gamma * max(Q[sp-1,:]) - Q[s-1,a-1])
-
-def create_policy_file(Q, n_states, n_actions):
-    """
-    CREATE .POLICY FILE:
-    Given the state action function array, creates and saves a .policy CSV-style file where the number of rows equals 
-    the number of states and the number of columns equals the number of actions.
-    Param: Q: (np array of size (n_states, n_actions)) action value function
-    """
-    if n_states == const.LARGE_N_STATES:
-        filename = "large.policy"
-    elif n_states == const.MEDIUM_N_STATES:
-        filename = "medium.policy"
-    else:
-        filename = "small.policy"
-
-    policy = np.zeros((n_states, 1), dtype=int)
-    unexplored_states= np.where(~Q.any(axis=1))[0]  # Get row indices of rows that are all zeros (unexplored states)
-
-    for i in range(n_states):
-        if i in unexplored_states:  # If state is unexplored (i.e. the row is all zeros in Q), assign a random action to that state
-            policy[i, 0] = randint(1, n_actions)
-        else:
-            policy[i, 0] = int(np.where(Q == max(Q[i,:]))[1][0] + 1)  # Get action number of best action
-
-    np.savetxt(filename, policy, fmt="%i")
+        S, iterations = 100000, 35
+    Q = np.zeros((S,A))
+    policy = Q_learning(D, Q, discount, lr, iterations) + 1
+    np.savetxt(outfile,policy)
+    taken = time.time() - start
+    print("time taken for ",infile," = ",taken," seconds")
 
 
 def main():
-    
-    # Load data and gamma
-    data, gamma, n_states, n_actions = load_data()
-
-    m = len(data.index)  # m = number of samples in dataset
-    alpha = 1/m          # learning rate
-    Q = np.zeros((n_states, n_actions), dtype=float)
-
-    start_time = timeit.default_timer()
-    for i in range(m):
-        s = data.iloc[i]['s']
-        a = data.iloc[i]['a']
-        r = data.iloc[i]['r']
-        sp = data.iloc[i]['sp']
-        update(Q, s, a, r, sp, alpha, gamma)
-    stop_time = timeit.default_timer()
-    print("Training time: ", stop_time - start_time)
-
-    create_policy_file(Q, n_states, n_actions)
+    filenames = ['small', 'medium', 'large']
+    for filename in filenames:
+        compute(filename+'.csv', filename+'.policy')
 
 if __name__ == '__main__':
     main()
